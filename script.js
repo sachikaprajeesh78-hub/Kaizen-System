9186bf8db044b7109787fa23f47e0417d8dd2077
sachikaprajeesh78-hub <sachikaprajeesh78@gmail.com>
2026-07-06 16:52:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("submitBtn").addEventListener("click", function () {
+    window.open("https://docs.google.com/forms/d/e/1FAIpQLSf-czY82irT_5Phs_Ic2LSJUnIP0iGZE4p1F66mStj90rZO6g/viewform?usp=dialog", "_blank");
+});
